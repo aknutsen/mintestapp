8eb0ffe4075ea35145a82024cbdd8e423844b0b7
Anders Knutsen <andergk@gmail.com>
2019-05-03 11:08:29 +0200

    laget en ny rute i api-et

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ app.get('/', function (req, res) {
  res.send(JSON.stringify({ Hello: 'World'}));
 });
 
+
+app.get('/param/:first/:second', function (req, res) {
+    res.send(req.params)
+});
+
 app.listen(port, function () {
  console.log('Example app listening on port '+port+'!');
 });
